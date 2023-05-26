2f49c757254569033bdae0dd4520101e67171527
James Oku <Oku17002@byui.edu>
2023-05-26 15:35:15 -0600

    new

--- controllers/ingredientscon.ts
+++ controllers/ingredientscon.ts
@@ -9,6 +9,15 @@ const Ingcon = {
     }
   },
 
+  getingredient: async (req, res) => {
+    try {
+      const foods = await Food.findById(req.params.id);
+      res.json(foods);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
   addingredients: async (req, res) => {
     try {
       const { name, ingredients } = req.body;
@@ -21,6 +30,51 @@ const Ingcon = {
       res.status(201).json(savedFood);
     } catch (err) {
       res.status(400).json({ message: err.message });
+    }
+  },
+
+  updateIngredients: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, ingredients } = req.body;
+
+      // Find the food object by id and update it
+      const updatedFood = await Food.findByIdAndUpdate(
+        id,
+        { name, ingredients },
+        { new: true }
+      );
+
+      // If the food was not found, send a 404 response
+      if (!updatedFood) {
+        return res.status(404).json({ message: "Food not found" });
+      }
+
+      // Respond with the updated food
+      res.status(200).json(updatedFood);
+    } catch (err) {
+      // If there was an error, respond with a 400 status and the error message
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  deleteFood: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      // Find the food object by id and remove it
+      const deletedFood = await Food.findByIdAndRemove(id);
+
+      // If the food was not found, send a 404 response
+      if (!deletedFood) {
+        return res.status(404).json({ message: "Food not found" });
+      }
+
+      // Respond with a success message
+      res.status(200).json({ message: "Food deleted successfully" });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Internal server error" });
     }
   },
 };
--- routes/ingridients.ts
+++ routes/ingridients.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 const router = Router();
 const bodyParser = require("body-parser");
 router.use(bodyParser.json());
-
 const ingcon = require("../controllers/ingredientscon");
 router.use("/", require("../routes/swagger"));
+//
 router.get("/ing", ingcon.getallingredients);
 router.post("/ing", ingcon.addingredients);
-
+//
+router.get("/ing/:id", ingcon.getingredient);
+router.put("/ing/:id", ingcon.updateIngredients);
+router.delete("/ing/:id", ingcon.deleteFood);
 module.exports = router;
